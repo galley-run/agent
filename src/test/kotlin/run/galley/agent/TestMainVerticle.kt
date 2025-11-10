90ffe5ae90af3c5f2f6fba7f0bcdd94b60c557a1
Patrick van Marsbergen <patrick@vanmarsbergen.com>
2025-11-10 11:52:34 +0100

    Add ktlint

--- src/test/kotlin/run/galley/agent/TestMainVerticle.kt
+++ src/test/kotlin/run/galley/agent/TestMainVerticle.kt
@@ -9,14 +9,19 @@ import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(VertxExtension::class)
 class TestMainVerticle {
-
   @BeforeEach
-  fun deploy_verticle(vertx: Vertx, testContext: VertxTestContext) {
+  fun deploy_verticle(
+    vertx: Vertx,
+    testContext: VertxTestContext,
+  ) {
     vertx.deployVerticle(MainVerticle()).onComplete(testContext.succeeding<String> { _ -> testContext.completeNow() })
   }
 
   @Test
-  fun verticle_deployed(vertx: Vertx, testContext: VertxTestContext) {
+  fun verticle_deployed(
+    vertx: Vertx,
+    testContext: VertxTestContext,
+  ) {
     testContext.completeNow()
   }
 }
